eb5913958fe0fcf2535d76a5cea86f052262efc1
Retky <jose.arredondo.a@outlook.com>
2022-02-28 15:40:04 -0600

    Modify 'script' syntax
    For linters

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-const burger = document.getElementById('menuBar')
-      menu = document.querySelector('.burguer_list')
-let menuClick = 0
+const burger = document.getElementById('menuBar');
+const menu = document.querySelector('.burguer_list');
+let menuClick = 0;
 
-burger.addEventListener('click', function(){
-  menu.classList.toggle('moveIn')
-  menuClick++
-  if(menuClick%2 != 0){
-    burger.src = 'img/Close.png'
+burger.addEventListener('click', () => {
+  menu.classList.toggle('moveIn');
+  menuClick += 1;
+  if (menuClick % 2 !== 0) {
+    burger.src = 'img/Close.png';
+  } else {
+    burger.src = 'img/Menu.png';
+    menuClick = 0;
   }
-  else {
-    burger.src = 'img/Menu.png'
-    menuClick = 0
-  }
-})
+});
 
-menu.addEventListener('click', function(){
-  menu.classList.remove('moveIn')
-  burger.src = 'img/Menu.png'
-  menuClick = 0
-})
+menu.addEventListener('click', () => {
+  menu.classList.remove('moveIn');
+  burger.src = 'img/Menu.png';
+  menuClick = 0;
+});
