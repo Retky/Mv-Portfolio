51ab57dbac11a579efc339bccfbc4bece14b9120
Retky <jose.arredondo.a@outlook.com>
2022-03-01 15:15:09 -0600

    Add 'liveView' button in popup box style

--- js/script.js
+++ js/script.js
@@ -23,12 +23,14 @@ menu.addEventListener('click', () => {
 
 // ↓ This is the basic Project Data Object.
 class ProjectBox {
-  constructor(name, tech, img, desc, live, src) {
+  constructor(name, tech, img, desc, liveTxt, liveSrc, liveImg, src) {
     this.name = name;
     this.techs = tech;
     this.image = img;
     this.description = desc;
-    this.live = live;
+    this.liveTxt = liveTxt;
+    this.liveSrc = liveSrc;
+    this.liveImg = liveImg;
     this.source = src;
   }
 }
@@ -39,46 +41,53 @@ const projectOne = new ProjectBox(
   ['html', 'Bootstrap', 'Ruby on rails'],
   'img/Snapshoot.png',
   'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  '#',
+  'See Live',
+  'https://github.com',
+  'img/LiveIco.png',
   'https://github.com',
 );
 
 // ↓Function to create the boxes
 function project(project){
-  let blurDiv = document.createElement('div');
+  const blurDiv = document.createElement('div');
   blurDiv.classList.add('popBlur');
-  let divBox = document.createElement('div');
+  const divBox = document.createElement('div');
   divBox.classList.add('popWindow');
   blurDiv.appendChild(divBox);
   // TITLE
-  let projectName = document.createElement('h3');
+  const projectName = document.createElement('h3');
   projectName.innerHTML = project.name;
   divBox.appendChild(projectName);
   // LIST
-  let techList = document.createElement('ul');
+  const techList = document.createElement('ul');
   techList.classList.add('languages');
   divBox.appendChild(techList);
   project.techs.forEach((lang) => {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.innerHTML = lang;
     li.classList.add('featLang');
     techList.appendChild(li);
   });
   // IMAGE
-  let divImg = document.createElement('div');
+  const divImg = document.createElement('div');
   divBox.appendChild(divImg);
-  let image = document.createElement('img')
+  const image = document.createElement('img')
   image.src = project.image;
   image.style.width = '100%';
   divImg.appendChild(image);
-  //DESCRIPTION
-  let divDesc = document.createElement('div');
+  // DESCRIPTION
+  const divDesc = document.createElement('div');
   divDesc.innerHTML = project.description;
   divBox.appendChild(divDesc);
-  // live Preview
-  const btnPreview = document.createElement('button');
-  btnPreview.href = project.live;
-  btnPreview.innerHTML = 'livePreview';
+  // LIVE PREVIEW
+  const btnPreview = document.createElement('a');
+  btnPreview.href = project.liveSrc;
+  btnPreview.classList.add('popBtn');
+  btnPreview.classList.add('orangeBtn');
+  btnPreview.innerHTML = project.liveTxt;
+  const liveIco = document.createElement('img');
+  liveIco.src = project.liveImg;
+  btnPreview.appendChild(liveIco);
   divBox.appendChild(btnPreview);
     // ↓ insert in the page
     document.body.appendChild(blurDiv);
